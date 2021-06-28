0c4b3142e7165eaede45d46aa685b0f82fc42b0e
adit_chheda <zaptower25@gmail.com>
2021-06-28 19:31:15 +0530

    settings.py

--- Profile/urls.py
+++ Profile/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from app1 import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +13,8 @@ urlpatterns = [
     path('app1/', include('app1.urls')),
     path('', views.home),
     path('search', views.search, name='search'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
